392df794d63c38f8f05999de6b0d11fd234e5a1f
arshdeep-singh6 <sin14582@sheridancollege.ca>
2025-02-06 13:38:15 -0500

    initialized weather app

---
+++ script.js
@@ -0,0 +1,33 @@
+const input = document.getElementById('search');
+const btn = document.getElementById('searchBtn');
+let temp;
+let desc;
+let windSpeed;
+let weatherIcon;
+btn.addEventListener('click', () => {
+    
+    const city = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eb297914d3b67b25d78c1e810fc3def3&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            temp = data.main.temp;
+            desc = data.weather[0].description;
+            windSpeed = data.wind.speed;
+            weatherIcon = data.weather[0].icon;
+
+            console.log('temp:', temp);
+            document.getElementById('temp').textContent = 'Temp: ' + temp + '°C';
+            document.getElementById('desc').textContent = desc;
+            document.getElementById('windSpeed').textContent = 'Wind Speed: ' + windSpeed + 'm/s';
+            document.getElementById('icon');
+            icon.src =  `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+        
+            
+});
